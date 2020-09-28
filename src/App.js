246482d1315fd4eeedac03db2c45f39db6dc1451
Kassam Ali Asif <asif.k.dev@gmail.com>
2020-09-28 18:00:57 +0200

    improvement nav + section 1

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import './App.css';
 
 import NavBarTop from './Components/NavBarTop';
+import SectionUn from './Components/SectionUn';
 
 const App = () => {
   return (
-    <div className="App container-fluid">
+    <div className="App container-fluid p-0 m-0">
       <NavBarTop />
       <main>
+        <SectionUn />
           
       </main>
 
---
+++ src/Components/SectionUn.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const SectionUn = () => {
+    return(
+        <section className="expand-lg" id="section-1">
+            <div className="section-1">
+                <img src="https://i.ibb.co/F0wMQTN/face-co.png" alt="avatar" id="avatar" />
+                <h1> KASSAMALI Asif </h1>
+                <span class="ligne"></span>
+                <h2> Développeur Web </h2>
+            </div>
+        </section>
+    )
+}
+
+export default SectionUn;
