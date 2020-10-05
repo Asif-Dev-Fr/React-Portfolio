02057f5a9ec74187692de05b4a541967740b10d6
Kassam Ali Asif <asif.k.dev@gmail.com>
2020-10-05 18:41:42 +0200

    Updates

---
+++ precache-manifest.7dc402ccf14ae5e205198a7cd7d75e3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "668a59f61c55fbd9e27557b029d2cc65",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "9453b738730d1ace4306",
+    "url": "/React-Portfolio/static/css/main.515c75c0.chunk.css"
+  },
+  {
+    "revision": "6640062a2254e7afbe27",
+    "url": "/React-Portfolio/static/js/2.4789a442.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Portfolio/static/js/2.4789a442.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9453b738730d1ace4306",
+    "url": "/React-Portfolio/static/js/main.21c4a298.chunk.js"
+  },
+  {
+    "revision": "7c35ff808cd94c4ce20c",
+    "url": "/React-Portfolio/static/js/runtime-main.0ad08de7.js"
+  }
+]);
